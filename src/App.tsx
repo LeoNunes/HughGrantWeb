a277efed3410ef1fefc13b69f296d8725740f21e
LeoNunes <leonardonunesdev@gmail.com>
2022-09-09 22:41:35 -0300

    Update project to use typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Ju and Matthias! We hava a react app hosted! =D
+        </p>
+        <p>
+          Now with TypeScript!
         </p>
       </header>
     </div>
