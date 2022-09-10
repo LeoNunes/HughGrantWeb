b08a6d814ae412c09f9abf1ea6ede66a61d3f15d
LeoNunes <leonardonunesdev@gmail.com>
2022-09-10 01:07:36 -0300

    Add redux with a counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented, amountAdded } from './features/counter/counterSlice';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const count = useAppSelector(state => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  // const handleClick = () => {
+  //   dispatch(incremented());
+  // }
+
+  const handleClick = () => {
+    dispatch(amountAdded(2));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClick}>
+          Olá {count}
+        </button>
         <p>
           Hello Ju and Matthias! We hava a react app hosted! =D
         </p>
